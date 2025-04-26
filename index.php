187a80d3d829649521104b3e46aedc99b08123e1
hmontes <haroldjosehd24@gmail.com>
2025-04-26 17:25:11 -0400

    Add new headers to allow HTTP requests only from 'http://localhost:5173' and enable sending cookies and authentication tokens with requests.

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin: http://localhost:5173');
+header('Access-Control-Allow-Credentials: true');
 header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Authorization");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, PATCH, DELETE");
 header("Allow: GET, POST, OPTIONS, PUT, DELETE");
